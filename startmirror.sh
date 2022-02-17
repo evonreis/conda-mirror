a34351e038ac27a3c220e8ac3e18697f36b8c5b8
Erik von Reis <evonreis@caltech.edu>
2022-02-16 20:07:08 -0800

    index repo when done mirroring

--- startmirror.sh
+++ startmirror.sh
@@ -6,6 +6,7 @@ src_dir=$1
 env_list_url=$2
 channel=$3
 repo_dir=$4
+conda_dir=$5
 
 # start python virtual environment
 source $src_dir/.venv/bin/activate
@@ -19,4 +20,9 @@ whitelist-environment $src_dir/environments/linux-64/* > $src_dir/config.yaml
 # mirror repo
 conda-mirror --upstream-channel $channel --target-directory $repo_dir --platform linux-64 --config $src_dir/config.yaml \
   --no-progress --include-depends
+
+source $conda_dir/bin/activate
+conda index $repo_dir
+conda deactivate
+
 
